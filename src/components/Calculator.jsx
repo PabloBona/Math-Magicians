92e96ab9d1a9473359b1dee54a494ebd3184651d
PabloBona <pablobonasera@gmail.com>
2023-07-05 18:47:41 -0300

    Add logic Folder Add onClick events Add handleButtonsClick function Add calculate.jsx & operate.jsx Refactor RwoButtons.jsx

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,22 +1,13 @@
-import FirstRowButtons from './FirstRowButtons';
-import FooterButtons from './FooterButtons';
-import ForthRowButton from './ForthRowButtons';
-import SecondRowButtons from './SecondRowButtons';
-import ThirdRowButton from './ThirdRowButtons';
+import Display from './Display';
+import RowButtons from './RowButtons';
 
 const Calculator = () => (
-  <>
+  <div className="container">
     <div className="row justify-content-center my-3">
-      <div className="col-8 col-6">
-        <div className=" d-flex justify-content-end bg-secondary text-light px-1 py-2">0</div>
-      </div>
-      <FirstRowButtons />
-      <SecondRowButtons />
-      <ThirdRowButton />
-      <ForthRowButton />
-      <FooterButtons />
+      <Display />
+      <RowButtons />
     </div>
-  </>
+  </div>
 );
 
 export default Calculator;
---
+++ src/components/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Display = () => (
+  <div className="col-8 col-6 d-flex justify-content-center">
+    <div className="display d-flex justify-content-end bg-secondary text-light px-1 py-2 w-100">
+      0
+    </div>
+  </div>
+);
+
+export default Display;
