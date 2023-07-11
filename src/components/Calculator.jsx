be37369916c052d56453ccf40547b42ef7fe6902
PabloBona <pablobonasera@gmail.com>
2023-07-11 16:45:54 -0300

    Add styles

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -14,7 +14,11 @@ const Calculator = () => {
     <div className="container">
       <div className="row my-3 text-light flex-column flex-md-row justify-content-center">
         <div className="col-md-6 my-3">
-          <h2 className="align-self-md-center">Let&apos;s do some Maths!</h2>
+          <div className="d-flex align-items-center">
+            {' '}
+            <h2 className="align-self-md-center fs-4">Let&apos;s do some Maths!</h2>
+            <img src={`${process.env.PUBLIC_URL}/magician.png`} className="w-25" alt="Magician" />
+          </div>
         </div>
         <div className="col-md-6 d-flex justify-content-center flex-wrap ">
           <Display calculatorData={calculatorData} />
